cd83fffcb277a0f38f48714d3f9c5ecf949157c2
vansh <vansha10@gmail.com>
2020-03-27 00:43:22 +0530

    first commit

---
+++ app/src/main/java/com/o/covid19volunteerapp/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.o.covid19volunteerapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.o.covid19volunteerapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
